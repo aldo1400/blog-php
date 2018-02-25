44a07be3587e000e4840714c73009ab0adf985e2
Aldo Choque <achoque1400@gmail.com>
2018-02-24 23:12:44 -0500

    logout final

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -11,7 +11,31 @@ class AuthController extends BaseController
     return $this->render('auth/login.twig');
   }
 
-  public function postLogin(){}
+  public function postLogin(){
+    $validator=new Validator();
+    $validator->add('email','required');
+    $validator->add('email','email');
+    $validator->add('password','required');
+
+    if($validator->validate($_POST)){
+        $user=User::where('email',$_POST['email'])->first();
+        if($user){
+          if(password_verify($_POST['password'],$user->password)){
+              $_SESSION['userId']=$user->id;
+              header('Location:'.BASE_URL.'admin');
+              return null;
+          }
+        }
+
+        //NOT OK
+
+        $validator->addMessage('email','Username and/or password doesnt not match');
+    }
+    $errors=$validator->getMessages();
+    return $this->render('auth/login.twig',[
+      'errors'=>$errors
+    ]);
+  }
 
   public function getLogout(){
     unset($_SESSION['userId']);
