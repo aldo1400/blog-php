30784d207360d579b9a256e248ddd4d5c177fdf9
Aldo Choque <achoque1400@gmail.com>
2018-02-24 21:48:27 -0500

    login

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php namespace App\Controllers;
+
+use App\Models\User;
+use Sirius\Validation\Validator;
+use App\Controllers\BaseController;
+
+class AuthController extends BaseController
+{
+  public function getLogin(){
+
+    return $this->render('login.twig');
+  }
+
+  public function postLogin(){
+    $validator=new Validator();
+    $validator->add('email','required');
+    $validator->add('email','email');
+    $validator->add('password','required');
+
+    if($validator->validate($_POST)){
+        $user=User::where('email',$_POST['email'])->first();
+        if($user){
+          if(password_verify($_POST['password'],$user->password)){
+              $_SESSION['userId']=$user->id;
+              header('Location:'.BASE_URL.'admin');
+              return null;
+          }
+        }
+
+        //NOT OK
+
+        $validator->addMessage('email','Username and/or password doesnt not match');
+    }
+    $errors=$validator->getMessages();
+    return $this->render('login.twig',[
+      'errors'=>$errors
+    ]);
+  }
+
+}
+
+?>
